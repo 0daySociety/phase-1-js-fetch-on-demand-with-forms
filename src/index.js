f9d8f61903f1b238d0cec8e87fa375a1e90a8ff6
--replace <david.thuku@studentmoringa.school.com>
2023-06-25 19:02:06 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
-  
+ const inputForm =document.querySelector('form');
+ inputForm.addEventListener('submit',(e)=>{
+    console.log(e +"event is loaded")
+ let Input=document.querySelector("#searchByID")   
+   e.preventDefault();
+   e.target.children[1].value
+
+
+   //    console.log(e.target.children[1].value)
+//console.log(fetch(`http://localhost:3000/movies/${e.target.children[1].value}`))
+
+fetch(`http://localhost:3000/movies/${e.target.children[1].value}`)
+.then(response=>response.json())
+.then(data=>{
+    let title =document.querySelector("#title ")
+    let summary =document.querySelector("#summary")
+
+    title.innerText=data.title
+    summary.innerText=data.summary
+})
+
+
+ })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
